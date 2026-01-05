af1764e48855acb20638c0d2f103f1d224138c39
Karnav5622 <karnav5622@gmail.com>
2026-01-05 15:43:12 +0530

    Updated simplecode.cpp

--- Basics/simplecode.cpp
+++ Basics/simplecode.cpp
@@ -1,13 +1,21 @@
 //Accessing and updating a variable in c
 #include<iostream>
 using namespace std;
+// int main(){
+//     int num,x;
+//     cout<<"Enter a number\n";
+//     cin>>num;
+//     cout<<"Enter a number to update\n";
+//     cin>>x;
+//     num=x;
+//     cout<<"Updated number is\n"<<num;
+//     return 0;
+// }
+//Concept of Constant variable
 int main(){
-    int num,x;
-    cout<<"Enter a number\n";
-    cin>>num;
-    cout<<"Enter a number to update\n";
-    cin>>x;
-    num=x;
-    cout<<"Updated number is\n"<<num;
+    const int a=100;
+    cout<<"Enter a number";
+    a=200;//Generate error due to a is constant
+    cout<<a;
     return 0;
 }
